4fd73019bd6c98854d61e651e947ce66f3809d73
melon <integrex@yandex.ru>
2017-10-03 00:38:47 +0500

    Добавлены функции подсчета узлов и суммы путей

--- tree/tree.hs
+++ tree/tree.hs
@@ -15,7 +15,22 @@ getNodesCount Leaf = 1
 getNodesCount (Node x y) = (getNodesCount x) + (getNodesCount y) + 1
 
 
-main = do
-	let tree = createTree (createTree simpleTree Leaf) simpleTree
-	print(getNodesCount tree)
+getLeavesCount :: Tree -> Int
+getLeavesCount Leaf = 1
+getLeavesCount (Node x y) = (getLeavesCount x) + (getLeavesCount y)
 
+
+getPathSumAccumulate :: Tree -> Int -> Int
+getPathSumAccumulate Leaf _ = 0
+getPathSumAccumulate (Node Leaf Leaf) _ = 2
+getPathSumAccumulate (Node Leaf y) collected = 1 + (getLeavesCount y) + (getPathSumAccumulate y collected)
+getPathSumAccumulate (Node x Leaf) collected = (getLeavesCount x) + (getPathSumAccumulate x collected) + 1
+getPathSumAccumulate (Node x y) collected = (getLeavesCount x) + (getPathSumAccumulate x collected) + (getLeavesCount y) + (getPathSumAccumulate y collected)
+
+
+getPathSum :: Tree -> Int
+getPathSum tree = getPathSumAccumulate tree 0
+
+main = do
+	let tree = createTree simpleTree (createTree simpleTree Leaf)
+	print(getPathSum tree)
